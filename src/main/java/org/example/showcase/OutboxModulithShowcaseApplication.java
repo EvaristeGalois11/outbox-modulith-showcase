23d5196ca3286b8d69ecb68f6c14a07bd216ff61
Claudio Nave <evaristegalois11@proton.me>
2025-03-31 21:42:29 +0200

    Set up project

---
+++ src/main/java/org/example/showcase/OutboxModulithShowcaseApplication.java
@@ -0,0 +1,13 @@
+package org.example.showcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutboxModulithShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutboxModulithShowcaseApplication.class, args);
+	}
+
+}
